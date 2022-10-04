71d665b05c239d4bf9156f77da1304b279f84be0
gherlea <101740436+gherlea@users.noreply.github.com>
2022-10-04 22:25:37 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello world!!!");
+    int x = 2;
+    x +=x+=x++ + ++x;
+    x*=x;
+
+
+    double y = 2.4;
+
+    long z = 2L;
+    float a = 2.4F;
+    double result = x +z;
+
+        System.out.println(result);
+        System.out.println(x + z);
+        System.out.println("Valoarea lui x este: " + x);
+        System.out.println(x + z +" este final " + 2);
+
+
+    }
+}
